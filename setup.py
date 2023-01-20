56baa34f4bb16b1e6f0d39c46172afcf60a2c4c0
Bulat <42501371+bulatnv@users.noreply.github.com>
2023-01-20 11:54:58 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __version__ = '2.8.0'
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open('README.rst', encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 # get the dependencies and installs
